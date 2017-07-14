8d19005e03bfdeaca99a96ffb0f4c69a96ea0c99
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-07-14 13:23:44 -0400

    Estructura HTML y estilos css de la sección Perfil listos. Agregadas algunas funcionalidades del JS como validación y localStorage de email y tarjetas.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,52 @@
-//Menú lateral retractil:
 $(document).ready(function(){
-    $(".button-collapse").sideNav();
-  })
+  	/* Menú retractil */
+  	$(".button-collapse").sideNav();
+  	$('.collapsible').collapsible();
+  	/* SELECT */
+  	$('select').material_select();
+
+	/* Validación Index */
+	$("#sesion").click(function(event){
+    if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#mail").val()))){
+   			$("#mail").append($("#mail").val("Error"));
+  		}
+  		if(!(/^\d{8}([0-9])*$/.test($("#pass").val())) ){
+   			$("#pass").append($("#pass").val("Error"));
+  		}
+  		else{
+  			$("#sesion").attr("href","home.html");
+        localStorage.setItem('mail',$("#mail").val());
+        localStorage.setItem('num-tarjeta',$("#pass").val());       
+  		}
+	});
+
+  
+  	/* Mostrar e-mail en perfil */
+  	var mail = localStorage.getItem('mail');
+  	$('#correo-ingresado').html(mail); 
+
+ 	var arr = [];
+
+  	/* Capturar valor Tarjetas BIP */
+  	$("#btn-tarjeta").click(function() {
+    if(!(/^\d{8}([0-9])*$/.test($("#tarjeta").val())) ){
+        $("#tarjeta").append($("#tarjeta").val("Error"));
+    } else {
+
+    var tarjetaIngresada = $("#tarjeta").val();
+    arr.push(tarjetaIngresada);
+    console.log(arr);
+
+    $("#tarjetas-creadas").append(`
+       <div class="caja">
+         <h6>`+ tarjetaIngresada +`</h6>
+       </div>
+     `);
+      
+    localStorage.setItem('todas-tarjetas',arr);
+    console.log(localStorage.getItem('todas-tarjetas'));
+    $("#tarjeta").val("");       
+    }
+  });
+
+});
